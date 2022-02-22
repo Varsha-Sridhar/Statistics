b5498abd07e0ade84071b93951af1ade326e82dc
Varsha Sridhar <69775069+Varsha-Sridhar@users.noreply.github.com>
2022-02-22 09:54:05 -0500

    Create BestSubsetVariables.R

---
+++ BestSubsetVariables.R
@@ -0,0 +1,18 @@
+#function to show best subset variables based on the best regsubsets variable. 
+#input should be the result from regsubsets()
+
+BestSubsetVariables = function(regout){
+  summary_reg <- summary(regout)
+  
+  #find the best variables based on best r-squared 
+  truevariables = data.frame(summary_reg$which[which.min(summary_reg$cp),][summary_reg$which[which.min(summary_reg$cp),] == TRUE])
+  
+  #find row that gives intercept and remove it from the list 
+  intercept = which(rownames(truevariables) == "(Intercept)")
+  truevariables =subset(truevariables, rownames(truevariables)!="(Intercept)")
+  
+  #paste the variables together with a +  
+  bestvariables <- noquote(paste(noquote(rownames(truevariables)), collapse = "+"))
+
+  return(bestvariables) 
+}
