93ad7d4e626a5f729562700c9c89a04a99935325
Varsha Sridhar <69775069+Varsha-Sridhar@users.noreply.github.com>
2022-02-22 09:43:56 -0500

    Create BestSubsets.R
    #function to show a linear model based on the best regsubsets variable. 
    #Note: Make sure first input variable in the function is in quotes.
    #For Example: LinearModelBestSubsets("Price", regout)
    # second input should be the result from regsubsets()

---
+++ BestSubsets.R
@@ -0,0 +1,25 @@
+#function to show a linear model based on the best regsubsets variable. 
+#Note: Make sure first input variable in the function is in quotes.
+#For Example: LinearModelBestSubsets("Price", regout)
+# second input should be the result from regsubsets
+
+BestSubsets = function(y, regout){
+  y_var <- noquote(y)
+  summary_reg <- summary(all)
+  
+  #find the best variables based on best r-squared 
+  truevariables = data.frame(summary_reg$which[which.min(summary_reg$cp),][summary_reg$which[which.min(summary_reg$cp),] == TRUE])
+  
+  #find row that gives intercept and remove it from the list 
+  intercept = which(rownames(truevariables) == "(Intercept)")
+  truevariables =subset(truevariables, rownames(truevariables)!="(Intercept)")
+  
+  #paste the variables together with a +  
+  #create a linear model with the best subsets 
+  model <- noquote(paste(
+    y, 
+    "~", 
+    paste(variables, collapse = " + ")))
+  
+  return(model) 
+}
